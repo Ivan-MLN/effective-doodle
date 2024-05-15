f4aba8f5842cc4f49d6e1244e1419bbaca0c07df
Ivanzz <78632369+Ivan-MLN@users.noreply.github.com>
2024-05-15 22:47:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
+function unduh() {
+    const url = document.getElementById('urlInput').value;
+    if (url) {
+        const tiktok = new TikTokDownloader();
+        return tiktok.download(url);
+    } else {
+        alert('Harap masukkan URL TikTok.');
+    }
+}
+
+
+
+
 class TikTokDownloader {
   constructor() {
     this.baseUrl = "https://api.tiklydown.eu.org/api";
   }
-
   async download(url) {
       const res = await axios.get(`${this.baseUrl}/download?url=${url}`);
       return res.data;
