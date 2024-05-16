dc09db439e8185b16b214d28390667c797ba335b
Ivanzz <78632369+Ivan-MLN@users.noreply.github.com>
2024-05-16 09:04:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ class TikTokDownloader {
     this.baseUrl = "https://api.tiklydown.eu.org/api";
   }
   async download(url) {
-      const res = await axios.get(`${this.baseUrl}/download?url=${url}`);
-      return res.data;
+      const res = await axios.get(this.baseUrl + "/download?url=" + url);
+      return res.data.video.noWatermark
   }
 }
