026454d3f94ba7bbf5d57d8b759b914c2d6b5a8c
Ivanzz <78632369+Ivan-MLN@users.noreply.github.com>
2024-05-20 13:13:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,23 @@
-async function unduh(type) {
+async function unduh() {
     const url = document.getElementById('urlInput').value;
-    if (url && url.includes("tiktok.com")) {
+    if (url) {
         const tiktok = new TikTokDownloader();
         const result = await tiktok.download(url);
-        const { video2, video_hd, music } = result.result;
-        
-        if (!type) {
-            document.getElementById("submit").style.display = "none";
-            document.getElementById("vd").style.display = "block";
-            document.getElementById("hd").style.display = "block";
-            document.getElementById("mp3").style.display = "block";
-            document.getElementById("vid").style.display = "block";
-            document.getElementById("vidSource").src = video2; 
-            document.getElementById("vid").load(); 
-        } else {
-            switch (type) {
-                case "sd":
-                    window.location.href = video2;
-                    return video2;
-                case "hd":
-                    window.location.href = video_hd;
-                    break;
-                case "mp3":
-                    window.location.href = music;
-                    break;
-            }
-        }
+        window.location.href = result;
     } else {
-        alert('Harap masukkan URL TikTok dengan benar!');
+        alert('Harap masukkan URL TikTok.');
     }
 }
 
+
+
+
 class TikTokDownloader {
-    constructor() {
-        this.baseUrl = "https://api.tiklydown.eu.org/api";
-    }
-    async download(url) {
-        const res = await axios.get(this.baseUrl + "/download/v2?url=" + url);
-        return res.data;
-    }
+  constructor() {
+    this.baseUrl = "https://api.tiklydown.eu.org/api";
+  }
+  async download(url) {
+      const res = await axios.get(this.baseUrl + "/download?url=" + url);
+      return res.data.video.noWatermark;
+  }
 }
