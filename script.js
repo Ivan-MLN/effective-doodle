dfbcc06035b547ba7c45411b15c96d123efcf0ed
Ivanzz <78632369+Ivan-MLN@users.noreply.github.com>
2024-05-20 13:06:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,42 @@
 async function unduh(type) {
     const url = document.getElementById('urlInput').value;
-   // if (url && url.includes("tiktok.com")) {
+    if (url && url.includes("tiktok.com")) {
         const tiktok = new TikTokDownloader();
-        const { video2, video_hd, music } = await (await tiktok.download(url)).result
+        const result = await tiktok.download(url);
+        const { video2, video_hd, music } = result.result;
+        
         if (!type) {
-            document.getElementById("submit").style.display = "hidden"
-            document.getElementById("vd").style.display = "block"
-            document.getElementById("hd").style.display = "block"
-            document.getElementById("mp3").style.display = "block"
-            document.getElementById("vid").style.display = "block"
+            document.getElementById("submit").style.display = "none";
+            document.getElementById("vd").style.display = "block";
+            document.getElementById("hd").style.display = "block";
+            document.getElementById("mp3").style.display = "block";
+            document.getElementById("vid").style.display = "block";
+            document.getElementById("vidSource").src = video2; 
+            document.getElementById("vid").load(); 
         } else {
-        switch (type) {
-            case "sd":
-            window.location.href = video2
-            return video2
-            break 
-            case "hd":
-            window.location.href = video_hd
-            case "mp3":
-            window.location.href = json.result.music
-            break
+            switch (type) {
+                case "sd":
+                    window.location.href = video2;
+                    return video2;
+                case "hd":
+                    window.location.href = video_hd;
+                    break;
+                case "mp3":
+                    window.location.href = music;
+                    break;
+            }
         }
-      }
-  //  } else {
-    //    alert('Harap masukkan URL TikTok dengan benar!');
-   // }
+    } else {
+        alert('Harap masukkan URL TikTok dengan benar!');
+    }
 }
 
-
-
-
 class TikTokDownloader {
-  constructor() {
-    this.baseUrl = "https://api.tiklydown.eu.org/api";
-  }
-  async download(url) {
-      const res = await axios.get(this.baseUrl + "/download/v2?url=" + url);
-      return res.data;
-  }
+    constructor() {
+        this.baseUrl = "https://api.tiklydown.eu.org/api";
+    }
+    async download(url) {
+        const res = await axios.get(this.baseUrl + "/download/v2?url=" + url);
+        return res.data;
+    }
 }
