8058750e293bb8b9feb0611357fc3582c9d13a96
Ivanzz <78632369+Ivan-MLN@users.noreply.github.com>
2024-05-21 11:43:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
-const url = document.getElementById('urlInput').value;
-
 async function unduh() {
-    const url = document.getElementById('urlInput').value;
-    if (url && /^(https?:\/\/)?(www\.|vt\.|vm\.)?tiktok\.com\/.*$/.test(url)) {
-        const tiktok = new TikTokDownloader();
-        const json = await tiktok.download(url);
-        window.location.href = json.result.video2
+    const urlInput = document.getElementById('urlInput').value;
+    if (urlInput && /^(https?:\/\/)?(www\.|vt\.|vm\.)?tiktok\.com\/.*$/.test(urlInput)) {
+        const Tiktok = new TikTokDownloader();
+        const { result } = await Tiktok.download(urlInput);
+        window.location.href = result.video2;
     } else {
         alert('Harap masukkan URL TikTok dengan benar!');
     }
 }
 
-
-
-
 class TikTokDownloader {
-  constructor() {
-    this.baseUrl = "https://api.tiklydown.eu.org/api";
-  }
-  async download(url) {
-      const res = await axios.get(this.baseUrl + "/download/v2?url=" + url);
-      return res.data;
-  }
+    constructor() {
+        this.baseUrl = "https://api.tiklydown.eu.org/api";
+    }
+    async download(url) {
+        const { data } = await axios.get(`${this.baseUrl}/download/v2?url=${encodeURIComponent(url)}`);
+        return data;
+    }
 }
